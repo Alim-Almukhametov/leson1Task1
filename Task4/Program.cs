d1b7376b09e2591d2c07d6888a89ce6c7a5c3f94
Alim-Almukhametov <Swain221221@mail.ru>
2024-03-13 08:22:49 +0500

    add task4Lesson1

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 4;
+int b = 5;
+int c = 69;
+int d = 29;
+int e = 34;
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+if(d > max)
+{
+    max = d;
+}
+if(e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
